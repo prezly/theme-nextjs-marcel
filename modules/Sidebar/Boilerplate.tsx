6d3b143dd5e1bde44548ae67138268c49104c3c2
Sato <sato.rc@yandex.ru>
2021-04-07 16:13:38 +0300

    Base layout for Boilerplate and Social icons

--- modules/Sidebar/Boilerplate.tsx
+++ modules/Sidebar/Boilerplate.tsx
@@ -4,9 +4,35 @@ type Props = {
     newsroom: Newsroom;
 };
 
+// TODO: Find a way to fetch the boilerplate
+const MOCKED_BOILERPLATE = (
+    <>
+        There&apos;s a lot of bad news in the world, so we wanted to take a moment to focus on the good things in life,
+        {' '}
+        because even in the darkest night there are stars.
+        <br />
+        <br />
+        Have a nice story to recommend? Drop us a line!
+    </>
+);
+
+const MOCKED_ADDRESS = '3517 W. Gray St. Utica, Pennsylvania 57867';
+
 const Boilerplate = ({ newsroom }: Props) => (
-    <div>
-        TODO: Boilerplate
+    <div className="mb-12">
+        <h2 className="text-lg leading-6 mb-4 font-bold text-gray-300">
+            About
+            {' '}
+            {newsroom.display_name}
+        </h2>
+
+        <p className="text-gray-400 leading-7 mb-4">
+            {MOCKED_BOILERPLATE}
+        </p>
+
+        <address className="text-gray-400 leading-7 not-italic">
+            {MOCKED_ADDRESS}
+        </address>
     </div>
 );
 
--- modules/Sidebar/SocialLinks.tsx
+++ modules/Sidebar/SocialLinks.tsx
@@ -1,12 +1,24 @@
 import { Newsroom } from '@prezly/sdk/dist/types';
+import Icon from '@/components/Icon';
 
 type Props = {
     newsroom: Newsroom;
 };
 
 const SocialLinks = ({ newsroom }: Props) => (
-    <div>
-        TODO: Social Links
+    <div className="flex items-center">
+        <a className="mr-6 text-gray-400" href="https://discord.com/">
+            <Icon name="social-discord" />
+        </a>
+        <a className="mr-6 text-gray-400" href="https://discord.com/">
+            <Icon name="social-instagram" />
+        </a>
+        <a className="mr-6 text-gray-400" href="https://discord.com/">
+            <Icon name="social-twitch" />
+        </a>
+        <a className="mr-6 text-gray-400" href="https://discord.com/">
+            <Icon name="social-twitter" />
+        </a>
     </div>
 );
 
