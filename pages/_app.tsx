30120cf9a37e2f6038bcffd5ae4e5700b4924810
Sato <sato.rc@yandex.ru>
2021-04-07 18:42:23 +0300

    Restructure slate override styles, add more overrides

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '../styles/globals.css';
+import '../styles/slate-override.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     // eslint-disable-next-line react/jsx-props-no-spreading
