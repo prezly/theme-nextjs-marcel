9946136881288e3b510fc7c5666cc5ac2777e7a2
Sato <sato.rc@yandex.ru>
2021-04-07 18:49:21 +0300

    Handle the case with too many categories in Header

--- modules/Categories/Categories.tsx
+++ modules/Categories/Categories.tsx
@@ -1,19 +1,43 @@
 import React, { FunctionComponent } from 'react';
 import type { Category } from '@prezly/sdk/dist/types';
+
+import Icon from '@/components/Icon';
 import CategoryComponent from '../Category';
 
 type Props = {
     categories: Category[];
 };
 
-const Categories: FunctionComponent<Props> = ({ categories }) => (
-    <ul className="lg:flex lg:uppercase">
-        {categories.map((c) => (
-            <li key={c.id} className="mx-2">
-                <CategoryComponent category={c} />
-            </li>
-        ))}
-    </ul>
-);
+const MAX_DISPLAYED_CATEGORIES = 5;
+
+const Categories: FunctionComponent<Props> = ({ categories }) => {
+    const displayedCategories = categories.length > MAX_DISPLAYED_CATEGORIES
+        ? categories.slice(0, MAX_DISPLAYED_CATEGORIES)
+        : categories;
+    const extraCategories = categories.length > MAX_DISPLAYED_CATEGORIES
+        ? categories.slice(MAX_DISPLAYED_CATEGORIES)
+        : [];
+
+    return (
+        <ul className="lg:flex lg:uppercase lg:items-center">
+            {displayedCategories.map((c) => (
+                <li key={c.id} className="mx-2">
+                    <CategoryComponent category={c} />
+                </li>
+            ))}
+            {extraCategories.length && (
+                <button
+                    type="button"
+                    title="Toggle mobile menu"
+                    aria-label="Toggle mobile menu"
+                    className="px-3 py-2 flex items-center"
+                >
+                    <Icon name="menu" />
+                    <span className="hidden md:block uppercase ml-2">More categories</span>
+                </button>
+            )}
+        </ul>
+    );
+};
 
 export default Categories;
