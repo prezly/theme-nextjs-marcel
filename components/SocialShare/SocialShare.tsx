2374caae44accdfba586400b3be4ed326af02eb2
Sato <sato.rc@yandex.ru>
2021-04-24 12:58:47 +0300

    Disable unused vars rule for TODO stuff

--- components/SocialShare/SocialShare.tsx
+++ components/SocialShare/SocialShare.tsx
@@ -7,6 +7,7 @@ interface Props {
     story: Story;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const SocialShare: FunctionComponent<Props> = ({ story }) => (
     <div className="flex items-center -mx-1.5 lg:mx-0">
         <SocialLink iconName="social-facebook" link="https://discord.com/" />
