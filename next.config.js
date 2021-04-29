bc7b6eafc6940c1f4bd2f7a336430f3a8ff6271d
Sato <sato.rc@yandex.ru>
2021-04-29 13:23:43 +0300

    Update package-lock and next config after update from starter theme
    Feature/webpack 5

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  webpack: (config, {isServer}) => {
-
-    if (!isServer) {
-      config.node = { fs: 'empty' }
-    }
-
-    return config
-  },
   images: {
     domains: ['cdn.uc.assets.prezly.com']
   }
