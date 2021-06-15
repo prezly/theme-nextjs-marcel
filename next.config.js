48ee85d238aeead88e15e08caa1c55c7724bb733
Camil Blanaru <camil@edka.io>
2021-06-15 14:02:58 +0200

    Set Cache-Control header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   images: {
     domains: ['cdn.uc.assets.prezly.com']
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 }
