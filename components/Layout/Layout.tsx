2240e8fff328c801c3c301cfc07955b1ff965dfa
Francis Gyimah <fgyimah883@gmail.com>
2022-04-14 12:26:28 +0000

    Removed layout padding

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import Header from './Header';
 
 function Layout({ children }: PropsWithChildren<{}>) {
     return (
-        <div className="px-6 md:px-10 lg:max-w-[1040px] lg:mx-auto">
+        <div className="lg:max-w-[1040px] lg:mx-auto">
             <Header />
             {children}
         </div>
