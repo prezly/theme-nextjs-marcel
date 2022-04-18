6e78a7872bb60e5ef69dc7e9d63e0b2f44860392
Francis Gyimah <fgyimah883@gmail.com>
2022-04-18 18:52:40 +0000

    Updated contact card to match design

--- components/ContactCard/ContactCard.tsx
+++ components/ContactCard/ContactCard.tsx
@@ -1,5 +1,4 @@
 import type { PressContact } from '@prezly/slate-types';
-import Image from '@prezly/uploadcare-image';
 import classNames from 'classnames';
 
 import Icon from '@/components/Icon';
@@ -21,72 +20,69 @@ function ContactCard({ contact }: Props) {
     return (
         <div
             className={classNames(
-                'default-well text-gray-300 rounded-xl p-6',
-                'flex flex-col lg:flex-row items-center my-10',
+                'text-gray-300 rounded-xl p-6 border-[1px] border-gray-600',
+                'my-10',
             )}
         >
-            {avatar_url && (
-                <Image
-                    className="block w-20 h-20 rounded-full mb-6 lg:mb-0 lg:mr-8"
-                    src={avatar_url}
-                    alt={name}
-                    layout="fixed"
-                />
-            )}
-            <div>
-                <h4 className="text-lg text-gray-200 font-semibold mb-1">{name}</h4>
-                {subtitle && <h5 className="text-gray-400">{capitalizeFirstLetter(subtitle)}</h5>}
-
-                <div className="mt-6 lg:mt-4 flex flex-col lg:flex-row items-center">
-                    {email && (
-                        <a
-                            href={`mailto:${email}`}
-                            className="default-link lg:mr-6 flex items-center"
-                        >
-                            <Icon name="email" className="w-4 h-4 mr-2" />
-                            {email}
-                        </a>
-                    )}
-                    {website && (
-                        <a href={`${website}`} className="default-link lg:mr-6 flex items-center">
-                            <Icon name="web" className="w-4 h-4 mr-2" />
-                            {website}
-                        </a>
-                    )}
-                    {mobile && (
-                        <a
-                            href={`tel:${mobile}`}
-                            className="default-link lg:mr-6 flex items-center"
-                        >
-                            <Icon name="phone" className="w-4 h-4 mr-2" />
-                            {mobile}
-                        </a>
-                    )}
-                    {phone && (
-                        <a href={`tel:${phone}`} className="default-link lg:mr-6 flex items-center">
-                            <Icon name="phone" className="w-4 h-4 mr-2" />
-                            {phone}
-                        </a>
-                    )}
-                    {twitter && (
-                        <a
-                            href={`https://twitter.com/${twitter}`}
-                            className="default-link lg:mr-6 flex items-center"
-                        >
-                            <Icon name="social-twitter" className="w-4 h-4 mr-2" />
-                            {`@${twitter}`}
-                        </a>
-                    )}
-                    {facebook && (
-                        <a
-                            href={`https://facebook.com/${twitter}`}
-                            className="default-link lg:mr-6 flex items-center"
-                        >
-                            <Icon name="social-facebook" className="w-4 h-4 mr-2" />
-                            {`${facebook}`}
-                        </a>
+            <div className="flex items-center">
+                {avatar_url && (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                        className="block w-14 h-14 rounded mb-6 lg:mb-0 mr-4"
+                        src={avatar_url}
+                        alt={name}
+                    />
+                )}
+                <div>
+                    <h4 className="text-lg text-gray-200 font-semibold mb-1">{name}</h4>
+                    {subtitle && (
+                        <h5 className="text-gray-400">{capitalizeFirstLetter(subtitle)}</h5>
                     )}
                 </div>
+            </div>
+            <div className="mt-2 flex items-center flex-wrap">
+                {email && (
+                    <a href={`mailto:${email}`} className="default-link mr-6 flex items-center">
+                        <Icon name="email" className="w-4 h-4 mr-2" />
+                        {email}
+                    </a>
+                )}
+                {website && (
+                    <a href={`${website}`} className="default-link mr-6 flex items-center">
+                        <Icon name="web" className="w-4 h-4 mr-2" />
+                        {website}
+                    </a>
+                )}
+                {mobile && (
+                    <a href={`tel:${mobile}`} className="default-link mr-6 flex items-center">
+                        <Icon name="phone" className="w-4 h-4 mr-2" />
+                        {mobile}
+                    </a>
+                )}
+                {phone && (
+                    <a href={`tel:${phone}`} className="default-link mr-6 flex items-center">
+                        <Icon name="phone" className="w-4 h-4 mr-2" />
+                        {phone}
+                    </a>
+                )}
+                {twitter && (
+                    <a
+                        href={`https://twitter.com/${twitter}`}
+                        className="default-link mr-6 flex items-center"
+                    >
+                        <Icon name="social-twitter" className="w-4 h-4 mr-2" />
+                        {`@${twitter}`}
+                    </a>
+                )}
+                {facebook && (
+                    <a
+                        href={`https://facebook.com/${twitter}`}
+                        className="default-link mr-6 flex items-center"
+                    >
+                        <Icon name="social-facebook" className="w-4 h-4 mr-2" />
+                        {`${facebook}`}
+                    </a>
+                )}
             </div>
         </div>
     );
