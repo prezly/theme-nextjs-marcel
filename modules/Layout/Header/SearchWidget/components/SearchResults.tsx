e040adc88f578ba919de9b148e75c2066dc49c20
Francis Gyimah <fgyimah883@gmail.com>
2022-04-20 11:39:50 +0000

    Allowed full refresh on search

--- modules/Layout/Header/SearchWidget/components/SearchResults.tsx
+++ modules/Layout/Header/SearchWidget/components/SearchResults.tsx
@@ -1,8 +1,6 @@
 import type { AlgoliaStory } from '@prezly/theme-kit-nextjs';
 import translations from '@prezly/themes-intl-messages';
 import classNames from 'classnames';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import type { StateResultsProvided } from 'react-instantsearch-core';
 import { Hits } from 'react-instantsearch-dom';
 import { FormattedMessage } from 'react-intl';
@@ -19,9 +17,6 @@ type Props = Pick<StateResultsProvided<AlgoliaStory>, 'searchResults'> & {
 
 function SearchResults({ searchResults, query }: Props) {
     const { nbHits: totalResults } = searchResults;
-    const { asPath } = useRouter();
-
-    const isOnSearchPage = asPath.startsWith('/search');
 
     return (
         <div className="px-4 py-2">
@@ -31,18 +26,14 @@ function SearchResults({ searchResults, query }: Props) {
                 </p>
             )}
             <Hits hitComponent={Hit} />
-            {totalResults > 3 && isOnSearchPage ? (
-                <a href={`/search?query=${query}`} className={styles.link}>
+            {totalResults > 3 && (
+                <a
+                    href={`/search?query=${query}`}
+                    className={classNames(styles.link, 'text-primary mt-6 mb-3 uppercase')}
+                >
                     <span>View All</span>
                     <Icon name="arrow-right" className="w-3.5 h-3.5 ml-2" />
                 </a>
-            ) : (
-                <Link href={`/search?query=${query}`} passHref>
-                    <a className={classNames(styles.link, 'text-primary mt-6 mb-3 uppercase')}>
-                        <span>View All</span>
-                        <Icon name="arrow-right" className="w-3.5 h-3.5 ml-2" />
-                    </a>
-                </Link>
             )}
         </div>
     );
