b6e92d16d3e7630ed1d88e98cb3a359838c3c7e8
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-07-26 12:19:53 +0430

    Fix alignments in `Paragraph` component

--- components/RichText/Paragraph.tsx
+++ components/RichText/Paragraph.tsx
@@ -1,7 +1,25 @@
-import type { PropsWithChildren } from 'react';
+import { Alignment } from '@prezly/story-content-format';
+import type { ParagraphNode } from '@prezly/story-content-format';
+import classNames from 'classnames';
+import type { ReactNode } from 'react';
 
-function Paragraph({ children }: PropsWithChildren<{}>) {
-    return <p className="mb-3">{children}</p>;
+interface Props {
+    node: ParagraphNode;
+    children?: ReactNode;
+}
+
+function Paragraph({ children, node }: Props) {
+    return (
+        <p
+            className={classNames('mb-3', {
+                'text-left': node.align === Alignment.LEFT,
+                'text-center': node.align === Alignment.CENTER,
+                'text-right': node.align === Alignment.RIGHT,
+            })}
+        >
+            {children}
+        </p>
+    );
 }
 
 export default Paragraph;
