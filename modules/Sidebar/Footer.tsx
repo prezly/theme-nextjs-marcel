fefa250f0597a2f02b8721ed08d338a225be1646
Francis Gyimah <fgyimah883@gmail.com>
2023-06-23 08:05:15 +0000

    [DEV-11017] Updated event listener logic, dropped redundant powered by Prezly in sidebar

--- modules/Sidebar/Footer.tsx
+++ modules/Sidebar/Footer.tsx
@@ -1,32 +1,14 @@
 import { CookieConsentLink } from '@prezly/analytics-nextjs';
-import { useNewsroom } from '@prezly/theme-kit-nextjs';
-
-import { LogoPrezly } from '@/icons';
 
 import { DataRequestLink } from './DataRequestLink';
 
 function Footer() {
-    const newsroom = useNewsroom();
-
     return (
         <div className="mt-12">
             <div className="flex items-center">
                 <DataRequestLink className="mr-4 underline font-normal" />
                 <CookieConsentLink className="underline font-normal" />
             </div>
-            {!newsroom.is_white_labeled && (
-                <p className="mt-8 flex items-center">
-                    <span className="mr-2">Powered by</span>{' '}
-                    <a
-                        href="https://prez.ly/storytelling-platform"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-primary-lighter"
-                    >
-                        <LogoPrezly width={77} />
-                    </a>
-                </p>
-            )}
         </div>
     );
 }
